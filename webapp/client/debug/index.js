04e99e2e250ae9b94ab3fa589626cce7a25df055
Dmitry Rakov <rakov@mirapolis.ru>
2021-06-22 02:33:10 +0300

    use svg apply theory

--- webapp/client/debug/index.js
+++ webapp/client/debug/index.js
@@ -25,13 +25,26 @@ const template = `<div id="vue">
 <div style="display:flex"><button @click="render();">Строить</button><textarea v-model="param"></textarea></div>
 <div><textarea v-model="code" style="width:30em; height: 40em"></textarea></div>
 </div>
-<div class="view"><svg 
-	version="1.1"
-    baseProfile="full"
-    xmlns="http://www.w3.org/2000/svg"
-    :width="width" :height="height"
+<div class="view"><svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg"
+	:width="width" :height="height"
 	:viewBox="viewBox"
-><g transform="scale(1, -1)" v-html="svg"></g></svg></div>
+><g transform="scale(1, -1)" v-html="svg"></g></svg>
+<svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg" width="130mm" heigth="5mm" viewBox = "-50 0 130 5">
+<rect x="-50" y="0" width="10" height="5" class="level-50-fill" />
+<rect x="-40" y="0" width="10" height="5" class="level-40-fill" />
+<rect x="-30" y="0" width="10" height="5" class="level-30-fill" />
+<rect x="-20" y="0" width="10" height="5" class="level-20-fill" />
+<rect x="-10" y="0" width="10" height="5" class="level-10-fill" />
+<rect x="0" y="0" width="10" height="5" class="level0-fill" />
+<rect x="10" y="0" width="10" height="5" class="level10-fill" />
+<rect x="20" y="0" width="10" height="5" class="level20-fill" />
+<rect x="30" y="0" width="10" height="5" class="level30-fill" />
+<rect x="40" y="0" width="10" height="5" class="level40-fill" />
+<rect x="50" y="0" width="10" height="5" class="level50-fill" />
+<rect x="60" y="0" width="10" height="5" class="level60-fill" />
+<rect x="70" y="0" width="10" height="5" class="level70-fill" />
+</svg>
+</div>
 </div>`;
 
 const vm = new Vue({
